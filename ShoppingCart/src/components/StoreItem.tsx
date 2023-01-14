e9b3d3a007faa186d222cdbb07449aa0ad7f0b1d
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-01-13 20:27:08 -0500

    new vite app for youtube

--- ShoppingCart/src/components/StoreItem.tsx
+++ ShoppingCart/src/components/StoreItem.tsx
@@ -1,25 +1,40 @@
-import {Card, CardImg} from "react-bootstrap";
+import {Button, Card, CardImg} from "react-bootstrap";
+import {formatCurrency} from "../utils/formatCurrency";
 
-export function StoreItem(props : StoreItemImpl){
+export function StoreItem(props: StoreItemImpl) {
 
   // destructor the props
   const {id, name, price, imgUrl} = props
+  const quantity = 0;
 
   return (
-    <Card>
+    <Card className="h-100">
       <Card.Img src={imgUrl}/>
-      <p>{id}</p>
-      <p>{name}</p>
-      <p>{price}</p>
-      <p>{imgUrl}</p>
+      <Card.Body className="d-flex flex-column">
+
+        <Card.Title className="d-flex justify-content-between align-items-baseline
+        mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button>Add to Cart</Button>
+          ) : (
+            <div>
+            </div>
+          )}
+        </div>
+
+      </Card.Body>
     </Card>
   )
 
 }
 
 type StoreItemImpl = {
-    "id": number,
-    "name": string,
-    "price": number,
-    "imgUrl": string
+  "id": number,
+  "name": string,
+  "price": number,
+  "imgUrl": string
 }
--- ShoppingCart2/src/pages/Home.tsx
+++ ShoppingCart2/src/pages/Home.tsx
@@ -19,7 +19,6 @@ export function Home() {
         ))}
       </Row>
 
-
     </div>
   )
 
